429771ab8b37ac54c4344522d617a800ce9f3d08
JosiasAurel <ndjosiasaurel@gmail.com>
2021-05-08 04:08:50 +0100

    add ts

---
+++ client/components/Parent.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Parent = (): JSX.Element => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Parent;
---
+++ client/pages/index.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from "react";
+
+const Index: FunctionComponent = (): JSX.Element => {
+    return (
+        <div>
+            <h2>Hello</h2>
+        </div>
+    )
+}
+
+export default Index;
