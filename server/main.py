b93d0028c84a0d7eafc6af70612d7e107c186b41
JosiasAurel <ndjosiasaurel@gmail.com>
2021-05-18 19:00:48 +0100

    basic apis

--- server/main.py
+++ server/main.py
@@ -1,3 +1,17 @@
 from fastapi import FastAPI
+from .types import Member
+from .database import create_database, create_member, get_all_members, get_member
 
 app = FastAPI()
+
+
+@app.get("/members")
+async def give_members():
+    members = get_all_members()
+    return members
+
+
+@app.get("/members/{id}")
+async def throw_member(id: str):
+    member = get_member(id)
+    return member
