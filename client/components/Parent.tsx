5bc91acc220481cdd75b448d0e6c250320548b2d
JosiasAurel <ndjosiasaurel@gmail.com>
2021-05-19 01:34:35 +0100

    add parent component

---
+++ client/components/Parent.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from "react";
+
+
+const Parent: FunctionComponent = (): JSX.Element => {
+    return (
+        <div>
+            <h1>Hey</h1>
+        </div>
+    )
+}
+
+export default Parent;
